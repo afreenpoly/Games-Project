07d073d5e8bde4331717a6f466cd5b6801881f8a
Afreen Poly <85273664+afreenpoly@users.noreply.github.com>
2023-03-18 21:51:39 +0530

    Create index.js

---
+++ Dicegame/index.js
@@ -0,0 +1,14 @@
+randomNumber1 = Math.floor(Math.random() * 6) + 1;
+imageName1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src",imageName1);
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
+imageName2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src",imageName2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player2 Wins🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Tie";
+}
