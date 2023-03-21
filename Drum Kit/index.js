cadf310c728b07a9616bbe5cd9119fe713174d5d
afreenpoly <afreendeby@gmail.com>
2023-03-21 21:00:30 +0530

    added audio

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,3 +1,6 @@
 for (var index = 0; index < document.querySelectorAll(".drum").length; index++) {
-document.querySelectorAll(".drum")[index].addEventListener("click",function() {alert("hi");});
+document.querySelectorAll(".drum")[index].addEventListener("click",function() {
+    var audio=new Audio("sounds/tom-1.mp3")
+    audio.play();   
+});
 }
